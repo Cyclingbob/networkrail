6798ce223dccc65c3f54be1c1317759b4adc62ac
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2022-09-15 12:11:39 +0100

    Update module.js

--- module.js
+++ module.js
@@ -1,6 +1,7 @@
 const CORPUS = require('./CORPUS')
 const Schedule = require('./schedule')
 const Smart = require('./SMART')
+const TD = require('./TD')
 
 class Client {
     constructor(email, password){
@@ -8,7 +9,8 @@ class Client {
         this.password = password;
         this.Corpus = new CORPUS(this.email, this.password);
         this.Schedule = new Schedule(this.email, this.password);
-        this.Smart = new Smart(this.email, this.password)
+        this.Smart = new Smart(this.email, this.password);
+        this.TD = new TD(this.email, this.password);
     }
 }
 
