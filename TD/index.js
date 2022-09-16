f92ae360993124ffc8405aacbb419165d38a476b
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2022-09-16 10:10:59 +0100

    Update index.js

--- TD/index.js
+++ TD/index.js
@@ -86,4 +86,24 @@ class TD extends EventEmitter {
     }
 }
 
+TD.topics = {
+    All: "TD_ALL_SIG_AREA",
+    Scotland_East: "TD_SE_SIG_AREA",
+    Scotland_West: "TD_SW_SIG_AREA",
+    London_North_East: "TD_LNE_NE_SIG_AREA",
+    East_Midlands: "TD_MC_EM_SIG_AREA",
+    London_Great_Northern: "TD_LNE_GN_SIG_AREA",
+    London_North_Western_Lancashire: "TD_LNW_LC_SIG_AREA",
+    London_North_Western_Central: "TD_LNW_C_SIG_AREA",
+    London_West_Midlands_Central: "TD_LNW_WMC_SIG_AREA",
+    West_Coast_South: "TD_WCS_SIG_AREA",
+    Anglia: "TD_ANG_SIG_AREA",
+    Kent_And_Midlands: "TD_KENT_MCC_SIG_AREA",
+    Sussex: "TD_SUSS_SIG_AREA",
+    Wessex: "TD_WESS_SIG_AREA",
+    Western_Thames_Valley: "TD_WTV_SIG_AREA",
+    Western_West_Country: "TD_WWC_SIG_AREA",
+    Western_Wales: "TD_WWM_SIG_AREA"
+}
+
 module.exports = TD
